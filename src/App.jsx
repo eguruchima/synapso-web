788c53f2eff7eb94d9fa87c1b54f96dd9e437516
eguruchima <eguruchima@gmail.com>
2025-04-10 15:32:27 -0400

    finished signup function, no errors as of right now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { SignupPage } from "./SignupPage";
 
 axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.withCredentials = true;
@@ -9,6 +10,7 @@ function App() {
   return (
     <div>
       <Header />
+      <SignupPage />
       <Footer />
     </div>
   );
