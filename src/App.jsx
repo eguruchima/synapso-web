b95e1fc95d69a5975c3ceb38deb1a4ec5c5f5da4
eguruchima <eguruchima@gmail.com>
2025-04-11 02:14:11 -0400

    login signup and logout work

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { SignupPage } from "./SignupPage";
+import { LoginPage } from "./LoginPage";
+import { LogoutLink } from "./LogoutLink";
 
 axios.defaults.baseURL = "http://localhost:3000";
 axios.defaults.withCredentials = true;
@@ -11,6 +13,8 @@ function App() {
     <div>
       <Header />
       <SignupPage />
+      <LoginPage />
+      <LogoutLink />
       <Footer />
     </div>
   );
